f48e58b924c535f3d186dfe42fc57bbcd6e5fa64
Ashish <ashishpahwa7@gmail.com>
2016-05-21 04:18:44 +0530

    Implementation of Repeated Square method

---
+++ repeatedSquare.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+float expo(int x, int n){
+
+    if(n < 0) return expo(1/x,n);
+    if(n == 0) return 1;
+    if(n ==1) return x;
+
+    if(n%2==0) return expo(x*x, n/2);
+    if(n%2 !=0) return expo(x*x, (n-1)/2)*x;
+
+}
+
+main()
+{
+    printf("%f", expo(2,5));
+
+
+}
+
