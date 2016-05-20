756d419ae27b828ef8187637b667d82355257ec2
Ashish <ashishpahwa7@gmail.com>
2016-05-21 04:05:30 +0530

    Print Hello World to console

---
+++ HelloWorld.c
@@ -0,0 +1,11 @@
+/* A first program in C */
+
+#include<stdio.h>  // Library of standard input output functions
+
+main()   // First instruction is executed from main()
+{
+    printf("Hello,World! \n");  //prints text to screen 
+} 
+
+
+
