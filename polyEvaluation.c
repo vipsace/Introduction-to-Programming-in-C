e376ed72ef6a519e691d260cab8f07218effd555
Ashish <ashishpahwa7@gmail.com>
2016-05-21 04:17:25 +0530

    Evaluation of Polynomial using Horner's Method

---
+++ polyEvaluation.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int evalPolynomial(int *coef, int deg, int v){
+
+    int poly=coef[0], i;
+   
+    //Evaluate value of Polynomial using Horner's method
+
+     for(i=1 ;i <= deg; i++)
+         poly = poly*v + coef[i];
+
+
+    return poly;
+
+}
+
+int main(void)
+{
+    int i, coef[20], deg, result, v;
+
+    printf("Input degree of polynomial :");
+    scanf("%d",&deg);
+
+    for(i=0; i <= deg; i++)
+        scanf("%d",&coef[i]);
+
+    printf("Input the value of v :");
+    scanf("%d",&v);
+
+    result = evalPolynomial(coef, deg, v);
+
+    printf("%d",result);
+
+    return 0;
+
+}
+
+
